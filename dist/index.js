8897a1a9f612d0658cb1d90f4fa05480da6d0b24
wanghao <whateverhow@gmail.com>
2021-05-24 13:55:27 -0700

    [ignore-some-errors] 	new file:   dist/BaseBlockTracker.d.ts

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./BaseBlockTracker"));
+__export(require("./PollingBlockTracker"));
+__export(require("./SubscribeBlockTracker"));
+//# sourceMappingURL=index.js.map
